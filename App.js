5e42b8ed267e0bb4a088e8d9da51f19962e5e4d0
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-08 22:08:59 +0300

    Using the new Main component

--- App.js
+++ App.js
@@ -1,26 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from "./src/components/Main";
 
-export default function App() {
-  console.log('Testataan console.log');
-  // console.error('Testataan console.error');
-  return (
-    <View style={styles.container}>
-      <Text>
-        Open up App.js to start working on your app!
-		
-        Hei! 😸
-      </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <Main />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//😸
+
+export default App;
