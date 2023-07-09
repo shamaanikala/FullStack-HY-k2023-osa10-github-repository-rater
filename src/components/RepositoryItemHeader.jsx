7846434f2abd42302d48638043e27df755ee44ee
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-09 18:11:38 +0300

    10.5 developing Repo item header component

--- src/components/RepositoryItemHeader.jsx
+++ src/components/RepositoryItemHeader.jsx
@@ -1,11 +1,30 @@
 import { View, Image, StyleSheet } from "react-native";
+import Text from "./Text";
+import theme from "../theme";
 
 const styles = StyleSheet.create({
-  item: {
-    backgroundColor: 'gray',
+  headerContainer: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    backgroundColor: 'lightgray',
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16
+  },
+  avatarContainer: {
+
+  },
+  headerContentContainer: {
+    backgroundColor: 'lightblue',
+    // paddingTop: 5,
+    marginLeft: 10,
+  },
+  itemTitle: {
+    fontWeight: theme.fontWeights.bold,
+    fontSize: theme.fontSizes.subheading,
+  },
+  itemDescription: {
+    fontSize: theme.fontSizes.subheading,
   },
   ownerAvatar: {
     width: 50,
@@ -15,11 +34,17 @@ const styles = StyleSheet.create({
 
 const RepositoryItemHeader = ({ ownerAvatarUrl }) => {
   return (
-    <View>
-      <Image
-        style={styles.ownerAvatar}
-        source={{ uri: ownerAvatarUrl, }}
-      />
+    <View style={styles.headerContainer}>
+      <View style={styles.avatarContainer}>
+        <Image
+          style={styles.ownerAvatar}
+          source={{ uri: ownerAvatarUrl, }}
+        />
+      </View>
+      <View style={styles.headerContentContainer}>
+        <Text style={styles.itemTitle}>User/repository</Text>
+        <Text style={styles.itemDescription}>Repo description text </Text>
+      </View>
     </View>
   )
 }
