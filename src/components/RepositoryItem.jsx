6e4bfe2ea1917b699fa9758f1314c80e716650f6
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-09 22:54:42 +0300

    10.5 tidy up

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,34 +1,13 @@
 import { StyleSheet, View } from "react-native";
 import RepositoryItemHeader from "./RepositoryItemHeader";
-// import RepositoryItemContent from "./RepositoryItemContent";
 import RepositoryItemFooter from "./RepositoryItemFooter";
 
 // https://reactnative.dev/docs/flatlist?language=javascript
 const styles = StyleSheet.create({
   item: {
     backgroundColor: 'ivory',
-    // padding: 20,
-    // marginVertical: 5,
-    // marginHorizontal: 16
   },
 });
-
-/**
- * repoStrings object
- * 
- * Contains the strings that will be rendered
- * as keys to the repository values within
- * <FlatLine> renderItems.
- */
-// const repoStrings = {
-//   fullName: 'Full name',
-//   description: 'Description',
-//   language: 'Language',
-//   stargazersCount: 'Stars',
-//   forksCount: 'Forks',
-//   reviewCount: 'Reviews',
-//   ratingAverage: 'Rating',
-// }
 
 const RepositoryItem = (props) => {
   const repo = props.item;
@@ -41,14 +20,13 @@ const RepositoryItem = (props) => {
     forksCount,
     reviewCount,
     ratingAverage,
-    // eslint-disable-next-line no-unused-vars
     ...rest
   } = repo;
 
   return (
     <View style={styles.item}>
       <RepositoryItemHeader
-        avatarUrl={repo.ownerAvatarUrl}
+        avatarUrl={rest.ownerAvatarUrl}
         fullName={fullName}
         description={description}
         language={language}
