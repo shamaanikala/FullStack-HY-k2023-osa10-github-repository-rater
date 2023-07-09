ce52a641a1b5a5a3743b5631512750db6b00e378
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-09 19:34:48 +0300

    10.5 basic styles to RepoItem Footer, Content

--- src/components/RepositoryItemContent.jsx
+++ src/components/RepositoryItemContent.jsx
@@ -1,9 +1,21 @@
-import { View } from "react-native"
+import { View, StyleSheet } from "react-native"
 import Text from "./Text"
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    backgroundColor: 'lightgray',
+    padding: 10,
+    marginVertical: 5,
+    marginHorizontal: 16
+  },
+});
+
 
 const RepositoryItemContent = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Repository item content</Text>
     </View>
   );
--- src/components/RepositoryItemFooter.jsx
+++ src/components/RepositoryItemFooter.jsx
@@ -1,9 +1,20 @@
-import { View } from "react-native"
+import { View, StyleSheet } from "react-native"
 import Text from "./Text"
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    flexDirection: 'row',
+    backgroundColor: 'lightgray',
+    padding: 10,
+    marginVertical: 5,
+    marginHorizontal: 16
+  },
+});
 
 const RepositoryItemFooter = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Repository item footer</Text>
     </View>
   );
