3ae7fb46c2438cb464260720df9be4f722cc0e1e
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-10 15:19:03 +0300

    Ex10.8 simple login form added

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,37 @@
+import { View, StyleSheet, Pressable } from "react-native";
 import Text from "./Text";
+import FormikTextInput from "./FormikTextInput";
+import { Formik } from "formik";
+
+const styles = StyleSheet.create({});
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput name="password" placeholder="Password" secureTextEntry />
+      <Pressable onPress={onSubmit}>
+        <Text>Log in</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const initialValues = { username: '', password: '' };
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <View>
+      <Text>The sign-in view</Text>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+      </Formik>
+    </View>
+  );
 };
 
 export default SignIn;
