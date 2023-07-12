dd783fb969fb0ded4db54b052d76fc5c18bca046
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-12 19:52:03 +0300

    Insert ApolloClient to the app

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 // const NGROK_GRAPHQL = Constants.manifest.extra.NGROK_GRAPHQL;
 // const GRAPHQL_URL = `${NGROK_GRAPHQL}/graphql`;
-const GRAPHQL_URL = 'http://192.168.8.100:5000/graphql';
+const GRAPHQL_URL = 'http://192.168.8.100:4000/graphql';
 
 const httpLink = createHttpLink({
   uri: GRAPHQL_URL, 
