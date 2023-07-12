b282a8d45c64005d2c7a72fc1303c28f2062f049
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-12 16:39:42 +0300

    Add the NGROK graphql tunnel to the script

--- ngrokUtil.js
+++ ngrokUtil.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const { writeFile, open } = require('fs/promises');
 
 
-const NGROK_URL_VAR = 'NGROK_URL';
-
+const NGROK_API_URL_VAR = 'NGROK_URL';
+const NGROK_GRAPHQL_VAR = 'NGROK_GRAPHQL';
 /**
  * Makes an axios.get request to http://127.0.0.1:4040/api/tunnels
  * to find the local running NGRONK instance and parses the 
@@ -14,12 +14,17 @@ const NGROK_URL_VAR = 'NGROK_URL';
  * 
  * @returns string The url of a running NGRONK http tunnel.
  */
-const getNGROKurl = async () => {
+const getNgrokUrl = async (targetPort, ngrokPort=4040) => {
   try {
-    const response = await axios.get('http://127.0.0.1:4040/api/tunnels');
+    console.log(`Fetching NGROK tunnel from port ${ngrokPort}`);
+    const response = await axios.get(`http://127.0.0.1:${ngrokPort}/api/tunnels`);
     // url is found data.tunnels[].public_url
     console.log(response.data.tunnels.find(t => t.proto === 'https').public_url);
-    return response.data.tunnels.find(t => t.proto === 'https').public_url;
+    const tunnel = response.data.tunnels.find(t => t.proto === 'https')
+    if (tunnel.config.addr.endsWith(`:${targetPort}`)) {
+      console.log(tunnel.config.addr);
+      return response.data.tunnels.find(t => t.proto === 'https').public_url;
+    }
   } catch(error) {
     if (error.code === 'ECONNREFUSED') {
       console.error('Unable to connect ngrok server api!');
@@ -31,22 +36,23 @@ const getNGROKurl = async () => {
   }
 };
 
-const writeUrl = async (filename) => {
-  const url = await getNGROKurl();
+const writeUrl = async (variable, filename, targetPort, ngrokPort=4040) => {
+  const url = await getNgrokUrl(targetPort,ngrokPort);
   const file = await open(filename);
   let data = []; // skip the NGROK_URL
   for await (const line of file.readLines()) {
-    if (line.startsWith('NGROK_URL')) {
+    if (line.startsWith(variable)) {
       continue;
     }
-    data.push(line);
+    data.push(`${line}\n`);
   }
   if (!url) {
     // throw new Error('No ngrok url to write!');
     console.log('No ngrok url to write!');
     return;
   }
-  data.push(`${NGROK_URL_VAR}=${url}`);
+  data.push(`${variable}=${url}`);
+  console.log(data);
   try {
     await writeFile(filename,data);
   } catch (err) {
@@ -55,7 +61,10 @@ const writeUrl = async (filename) => {
   }
 };
 
-const run = async () => await writeUrl('.env');
+const run = async () => {
+  await writeUrl(NGROK_API_URL_VAR, '.env', 5000, 4040);
+  await writeUrl(NGROK_GRAPHQL_VAR, '.env', 4000, 4041);
+}
 
 run();
 
