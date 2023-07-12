efe079bc17d0705aa60fe9e2182b38db1aacb07a
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-12 16:45:48 +0300

    Add apolloClient.js

---
+++ src/utils/apolloClient.js
@@ -0,0 +1,18 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import Constants from 'expo-constants';
+
+const NGROK_GRAPHQL = Constants.manifest.extra.NGROK_GRAPHQL;
+const GRAPHQL_URL = `${NGROK_GRAPHQL}/graphql`;
+
+const httpLink = createHttpLink({
+  uri: GRAPHQL_URL, 
+});
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  });
+};
+
+export default createApolloClient;
