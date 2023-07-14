b5c5df69acf731ee908b1fd3a6dc4584a2e03675
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-14 10:16:09 +0300

    Ex10.14 authStorage.js initial state

---
+++ src/utils/authStorage.js
@@ -0,0 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class AuthStorage {
+  constructor(namespace = 'auth') {
+    this.namespace = namespace;
+  }
+
+  getAccessToken() {
+    // Get the access token for the storage
+  }
+
+  setAccessToken(accessToken) {
+    // Add the access token to the storage
+  }
+  
+  removeAccessToken() {
+    // Remove the access token from the storage
+  }
+}
+
+export default AuthStorage;
