bfba263af55bad1af1dccbc8d8a16393a0e667b0
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-14 20:32:54 +0300

    Greetings test example

--- src/__tests__/example.test.js
+++ src/__tests__/example.test.js
@@ -1,3 +1,24 @@
+import { Text, View } from "react-native";
+import { render, screen } from "@testing-library/react-native";
+
+const Greeting = ({ name }) => {
+  return (
+    <View>
+      <Text>Hello {name}!</Text>
+    </View>
+  );
+};
+
+describe('Greeting', () => {
+  it('renders a greetings message based on the name prop', () => {
+    render(<Greeting name="Kalle" />);
+
+    screen.debug();
+
+    expect(screen.getByText('Hello Kalle!')).toBeDefined();
+  });
+});
+
 describe('Example', () => {
   it('works', () => {
     expect(1).toBe(1);
