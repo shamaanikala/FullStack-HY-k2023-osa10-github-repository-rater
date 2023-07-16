60743f489f22b5d287aa2cea4174a50e523e26cd
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-16 16:58:35 +0300

    Ex10.19 add optional button to Repo Item view

--- src/components/RepositoryItem/index.jsx
+++ src/components/RepositoryItem/index.jsx
@@ -1,17 +1,22 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 import RepositoryItemHeader from "../RepositoryItemHeader";
 import RepositoryItemFooter from "../RepositoryItemFooter";
+import theme from "../../theme";
 
 // https://reactnative.dev/docs/flatlist?language=javascript
 const styles = StyleSheet.create({
   item: {
     backgroundColor: 'ivory',
   },
+  button: {
+    backgroundColor: theme.colors.primary,
+    borderRadius: 30,
+  },
 });
 
 const RepositoryItem = (props) => {
   const repo = props.item;
-
+  const viewSingleRepository = props.singleRepositoryView;
   const {
     fullName,
     description,
@@ -37,6 +42,13 @@ const RepositoryItem = (props) => {
         reviews={reviewCount}
         rating={ratingAverage}
       />
+      {viewSingleRepository &&
+        <Button
+          style={styles.button}
+          onPress={() => console.log('Open in GitHub pressed!')}
+          title="Open in GitHub"
+        />
+      }
     </View>
   );
 };
