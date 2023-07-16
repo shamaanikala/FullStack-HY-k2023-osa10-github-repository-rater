5633a442bcc14f0ade245a6b60f1535f2f634773
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-16 16:40:15 +0300

    Ex10.18 done

--- src/components/SignIn/SignIn.test.jsx
+++ src/components/SignIn/SignIn.test.jsx
@@ -2,11 +2,14 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react-nativ
 import { FormikSignInForm, initialValues, validationSchema } from ".";
 
 describe('SignIn', () => {
-  describe('SignInForm', () => {
-    it('calls onSubmit function with correct arguments when a valid form is submitted', async () => {
-      // render the SignInContainer/Form component, fill the text inputs and press the submit button
-      const onSubmit = jest.fn();
+  describe('FormikSignInForm', () => {
+    const onSubmit = jest.fn();
+    // disable useNativeDriver warning
+    // https://stackoverflow.com/questions/59587799/how-to-resolve-animated-usenativedriver-is-not-supported-because-the-native
+    jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
+    beforeEach(() => {
+      // render the SignInContainer/Form component, 
       render(
         <FormikSignInForm
           onSubmit={onSubmit}
@@ -14,7 +17,9 @@ describe('SignIn', () => {
           validationSchema={validationSchema}
         />
       );
-
+    });
+    it('calls onSubmit function with correct arguments when a valid form is submitted', async () => {
+      // fill the text inputs and press the submit button
       fireEvent.changeText(screen.getByPlaceholderText('Username'), 'elina');
       fireEvent.changeText(screen.getByPlaceholderText('Password'), 'password');
       fireEvent.press(screen.getByText('Sign in'));
@@ -22,6 +27,10 @@ describe('SignIn', () => {
       await waitFor(() => {
         // expect the onSubmit function to have been called once and with a correct first argument
         expect(onSubmit).toHaveBeenCalledTimes(1);
+        expect(onSubmit.mock.calls[0][0]).toEqual({
+          username: 'elina',
+          password: 'password',
+        });
       });
     });
   });
