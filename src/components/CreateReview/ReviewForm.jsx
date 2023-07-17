27ce7e88b15a6c150b1f1fa8c4aad4907cbf0420
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-17 17:50:11 +0300

    10.21 add yup validationSchema to form

--- src/components/CreateReview/ReviewForm.jsx
+++ src/components/CreateReview/ReviewForm.jsx
@@ -2,13 +2,26 @@ import { Button, StyleSheet, View } from "react-native"
 import FormikTextInput from "../FormikTextInput";
 import { Formik } from "formik";
 import theme from "../../theme";
+import * as yup from 'yup';
 
 const styles = StyleSheet.create({
   formContainer: {
+    padding: 10,
     backgroundColor: theme.colors.contentBackground,
   },
-  textInput: {},
-  button: {},
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 3,
+    borderColor: theme.colors.textSecondary,
+    paddingStart: 5,
+    paddingVertical: 5,
+    marginVertical: 5,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: theme.colors.primary,
+    borderRadius: 30,
+  },
 });
 
 const ReviewFormFields = ({ onSubmit }) => {
@@ -17,7 +30,10 @@ const ReviewFormFields = ({ onSubmit }) => {
       <FormikTextInput style={styles.textInput} name="repoOwnerName" placeholder="Reposity Owner name" />
       <FormikTextInput style={styles.textInput} name='repoName' placeholder="Reposity name" />
       <FormikTextInput style={styles.textInput} name='rating' placeholder="Rating between 0 and 100" />
-      <FormikTextInput style={styles.textInput} name="review" multiline placeholder="Review" />
+      <FormikTextInput style={styles.textInput}
+        name="review"
+        multiline
+        placeholder={`Type your repository\nreview here...`} />
       <Button
         style={styles.button}
         onPress={onSubmit}
@@ -26,6 +42,23 @@ const ReviewFormFields = ({ onSubmit }) => {
     </View>
   );
 };
+
+const validationSchema = yup.object().shape({
+  repoOwnerName: yup
+    .string()
+    .required('Reposity owner username is required'),
+  repoName: yup
+    .string()
+    .required('Repository name is required.'),
+  rating: yup
+    .number()
+    .integer('The rating must be an integer')
+    .min(0, 'Minimum rating value is ${min}')
+    .max(100, 'Maximum rating value is ${max}')
+    .required('Rating is required.'),
+  review: yup
+    .string(),
+});
 
 const ReviewForm = () => {
   const onSubmit = async (values) => {
@@ -38,7 +71,7 @@ const ReviewForm = () => {
       <Formik
         initialValues={{ repoOwnerName: '', repoName: '', rating: '', review: '' }}
         onSubmit={onSubmit}
-      // validationSchema={}
+        validationSchema={validationSchema}
       >
         {({ handleSubmit }) => <ReviewFormFields onSubmit={handleSubmit} />}
       </Formik>
