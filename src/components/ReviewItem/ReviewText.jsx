ad247ef80451fd0f7c908be8d7182dba36f45167
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-17 14:27:08 +0300

    10.20 review text layout

--- src/components/ReviewItem/ReviewText.jsx
+++ src/components/ReviewItem/ReviewText.jsx
@@ -1,17 +1,31 @@
 import { StyleSheet, View } from "react-native"
 import Text from "../Text"
+import theme from "../../theme";
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'cyan',
+    // backgroundColor: 'cyan',
     borderWidth: 1,
     borderStyle: 'dashed',
+    flexDirection: 'row',
+    flexGrow: 1,
+    backgroundColor: theme.colors.contentBackground, //'ivory',
+    paddingVertical: 15,
+    paddingStart: 20,
+    marginVertical: 5,
   },
   emptyBox: {
     backgroundColor: 'coral',
+    minWidth: 50,
   },
   textBox: {
     backgroundColor: 'cadetblue',
+    marginStart: 15,
+    marginRight: 20,
+    paddingHorizontal: 15,
+    alignItems: 'flex-start',
+    maxWidth: '89%',
+    // marginHorizontal: 15,
   },
 })
 const ReviewText = ({ text }) => {
