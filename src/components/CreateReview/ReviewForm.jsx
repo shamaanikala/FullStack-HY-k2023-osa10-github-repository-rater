34b02c549643ad1f0361fc952a8415f1881d8ac4
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-17 15:32:06 +0300

    10.21 adds a simple form draft

--- src/components/CreateReview/ReviewForm.jsx
+++ src/components/CreateReview/ReviewForm.jsx
@@ -1,8 +1,47 @@
-import { View } from "react-native"
+import { Button, StyleSheet, View } from "react-native"
+import FormikTextInput from "../FormikTextInput";
+import { Formik } from "formik";
+import theme from "../../theme";
+
+const styles = StyleSheet.create({
+  formContainer: {
+    backgroundColor: theme.colors.contentBackground,
+  },
+  textInput: {},
+  button: {},
+});
+
+const ReviewFormFields = ({ onSubmit }) => {
+  return (
+    <View style={styles.formContainer}>
+      <FormikTextInput style={styles.textInput} name="repoOwnerName" placeholder="Reposity Owner name" />
+      <FormikTextInput style={styles.textInput} name='repoName' placeholder="Reposity name" />
+      <FormikTextInput style={styles.textInput} name='rating' placeholder="Rating between 0 and 100" />
+      <FormikTextInput style={styles.textInput} name="review" multiline placeholder="Review" />
+      <Button
+        style={styles.button}
+        onPress={onSubmit}
+        title="Create a review"
+      />
+    </View>
+  );
+};
 
 const ReviewForm = () => {
+  const onSubmit = async (values) => {
+    console.log('create a new review pressed');
+    console.log(values);
+  };
+
   return (
     <View>
+      <Formik
+        initialValues={{ repoOwnerName: '', repoName: '', rating: '', review: '' }}
+        onSubmit={onSubmit}
+      // validationSchema={}
+      >
+        {({ handleSubmit }) => <ReviewFormFields onSubmit={handleSubmit} />}
+      </Formik>
     </View>
   );
 };
--- src/components/CreateReview/index.jsx
+++ src/components/CreateReview/index.jsx
@@ -1,11 +1,9 @@
 import { View } from "react-native"
-import Text from "../Text"
 import ReviewForm from "./ReviewForm";
 
 const CreateReview = () => {
   return (
     <View>
-      <Text>Create review component</Text>
       <ReviewForm />
     </View>
   );
