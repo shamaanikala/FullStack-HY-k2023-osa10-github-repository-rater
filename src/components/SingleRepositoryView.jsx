98a801790d9b30e787f366aadf786576fc77a3af
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-17 11:00:56 +0300

    10.20 render reviews

--- src/components/SingleRepositoryView.jsx
+++ src/components/SingleRepositoryView.jsx
@@ -1,7 +1,17 @@
 import { useParams } from "react-router-native";
 import RepositoryItem from "./RepositoryItem";
 import useRepository from "../hooks/useRepository";
-import { FlatList } from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
+import Text from "./Text";
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+  listHeader: {
+    marginBottom: 10,
+  },
+});
 
 // Single repository info
 const SingleRepositoryViewContainer = ({ repository }) => {
@@ -14,7 +24,21 @@ const SingleRepositoryViewContainer = ({ repository }) => {
 const ReviewItem = ({ review }) => {
   // sinlge review item
   console.log(review);
+  return (
+    <View id="review-container">
+      <View id="review-header">
+        <Text>{review.rating}</Text>
+        <Text>{review.user.username}</Text>
+        <Text>{review.createdAt}</Text>
+      </View>
+      <View id="review-content">
+        <Text>{review.text}</Text>
+      </View>
+    </View>
+  )
 }
+
+const ItemSeparator = () => <View style={styles.separator} />;
 
 const SingleRepositoryView = () => {
   const { repoId } = useParams();
@@ -32,6 +56,8 @@ const SingleRepositoryView = () => {
       renderItem={({ item }) => <ReviewItem review={item} />}
       keyExtractor={({ id }) => id}
       ListHeaderComponent={() => <SingleRepositoryViewContainer repository={repository} />}
+      ListHeaderComponentStyle={styles.listHeader}
+      ItemSeparatorComponent={ItemSeparator}
     // ...
     />
   );
