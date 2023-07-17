48b790faa08349bdf626431f3c41d5ad4f50bdb4
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-17 14:56:04 +0300

    Ex10.21 add <CreateReview>

---
+++ src/components/CreateReview/index.jsx
@@ -0,0 +1,12 @@
+import { View } from "react-native"
+import Text from "../Text"
+
+const CreateReview = () => {
+  return (
+    <View>
+      <Text>Create review component</Text>
+    </View>
+  );
+};
+
+export default CreateReview;
