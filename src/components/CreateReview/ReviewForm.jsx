68d663f9ae6d774c56b3b135a4580b853dfbc1a3
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-17 15:08:48 +0300

    10.21 create <ReviewForm>

---
+++ src/components/CreateReview/ReviewForm.jsx
@@ -0,0 +1,10 @@
+import { View } from "react-native"
+
+const ReviewForm = () => {
+  return (
+    <View>
+    </View>
+  );
+};
+
+export default ReviewForm;
--- src/components/CreateReview/index.jsx
+++ src/components/CreateReview/index.jsx
@@ -1,10 +1,12 @@
 import { View } from "react-native"
 import Text from "../Text"
+import ReviewForm from "./ReviewForm";
 
 const CreateReview = () => {
   return (
     <View>
       <Text>Create review component</Text>
+      <ReviewForm />
     </View>
   );
 };
