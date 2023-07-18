37ea4cb02e81e793a30cbcfd1f45ef6887452ded
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-18 09:41:38 +0300

    Ex10.22 add <SignUp>

---
+++ src/components/SignUp/index.jsx
@@ -0,0 +1,14 @@
+import { View } from "react-native"
+import Text from "../Text"
+
+const SignUp = () => {
+  return (
+    <View>
+      <Text>
+        Sign Up form here.
+      </Text>
+    </View>
+  );
+};
+
+export default SignUp;
