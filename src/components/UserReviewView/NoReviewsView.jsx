8931c92004af9b0c72869e34e68c96a571d909cc
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-19 17:51:31 +0300

    Ex10.25 done

--- src/components/UserReviewView/NoReviewsView.jsx
+++ src/components/UserReviewView/NoReviewsView.jsx
@@ -1,19 +1,40 @@
 import { StyleSheet, View } from "react-native"
 import Text from "../Text";
+import theme from "../../theme";
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'gold',
+    backgroundColor: theme.colors.contentBackground,
+    padding: 15,
   },
   firstLineContainer: {
-    backgroundColor: 'beige',
-    borderWidth: 1,
+    // backgroundColor: 'beige',
+    paddingVertical: 15,
+    alignItems: 'center',
+    // borderWidth: 1,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderTopWidth: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderColor: theme.colors.primary,
   },
   secondLineContainer: {
-    backgroundColor: 'cornsilk',
-    borderWidth: 1,
+    // backgroundColor: 'cornsilk',
+    alignItems: 'center',
+    // borderWidth: 1,
+    paddingHorizontal: 15,
+    paddingBottom: 20,
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderBottomWidth: 1,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    borderColor: theme.colors.primary,
   },
-  firstLineText: {},
+  firstLineText: {
+    paddingVertical: 10,
+  },
   secondLineText: {
     fontStyle: 'italic',
   },
@@ -23,13 +44,17 @@ const NoReviewsView = () => {
   return (
     <View style={styles.container}>
       <View style={styles.firstLineContainer}>
-        <Text style={styles.firstLineText}>
+        <Text fontSize="subheading" fontWeight="bold" style={styles.firstLineText}>
           You have not reviewed any GitHub repositories yet!
         </Text>
       </View>
       <View style={styles.secondLineContainer}>
         <Text style={styles.secondLineText}>
-          Create a new GitHub repository review from the `&quot;`Create a review `&quot;` tab.
+          Create a new GitHub repository review by pressing the
+        </Text>
+        <Text fontSize="subheading" fontWeight="bold" style={styles.firstLineText}>Create a review</Text>
+        <Text style={styles.secondLineText}>
+          tab from top navigation bar.
         </Text>
       </View>
 
