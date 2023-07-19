0a848c1c92bab6ff2f770863998d513fa324c8f6
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-19 15:20:37 +0300

    10.25 render reviews in <MyReviewsView>

--- src/components/UserReviewView/index.jsx
+++ src/components/UserReviewView/index.jsx
@@ -1,7 +1,21 @@
-import { View } from "react-native"
+import { FlatList, View, StyleSheet } from "react-native"
 import Text from "../Text"
 import { useQuery } from "@apollo/client";
 import { GET_SIGNED_USER } from "../../graphql/queries";
+import ReviewItem from "../ReviewItem";
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+  container: {
+    backgroundColor: 'pink',
+    padding: 15,
+  },
+});
+
+const ItemSeparator = () => <View style={styles.separator} />;
+
 
 const UserReviewView = () => {
   const userQuery = useQuery(GET_SIGNED_USER, {
@@ -10,15 +24,30 @@ const UserReviewView = () => {
 
   if (userQuery.loading) {
     console.log('loading...');
-  } else {
-    console.log(userQuery.data);
+    return (
+      <View style={styles.container}>
+        <Text>Loading user reviews...</Text>
+      </View>
+    );
   }
 
+  console.log('userQuery.data', userQuery.data);
+  const { username, reviews, ...rest } = userQuery.data.me;
+  const reviewItems = reviews
+    ? reviews.edges.map(edge => edge.node)
+    : [];
+  console.log(reviewItems);
   return (
-    <View>
-      <Text>
-        User Review View
+    <View style={styles.container}>
+      <Text fontSize="heading" fontWeight="bold">
+        {username} reviews:
       </Text>
+      <FlatList
+        data={reviewItems}
+        renderItem={({ item }) => <ReviewItem review={item} />}
+        keyExtractor={({ id }) => id}
+        ItemSeparatorComponent={ItemSeparator}
+      />
     </View>
   );
 };
