2918d1ecf5939cb87e38a857a14654ea1fcc97f6
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-19 11:57:55 +0300

    10.24 change component to class component

--- src/components/RepositoryList/RepositoryListHeaderContainer.jsx
+++ src/components/RepositoryList/RepositoryListHeaderContainer.jsx
@@ -1,23 +1,55 @@
+// define this component as class to fix the component
+// unmounting when using the filtering
+import React from 'react';
 import { View } from "react-native"
 import RepositoryListSortingPicker from "./RepositoryListSortingPicker"
 import RepositoryListFilter from "./RepositoryListFilter";
 
-const RepositoryListHeaderContainer = (stateProps) => {
-  const {
-    searchKeyword,
-    setSearchKeyword,
-    ...sortingProps
-  } = stateProps;
+export class RepositoryListHeaderContainer extends React.Component {
+  renderHeader = () => {
+    const stateProps = this.props;
+    const {
+      searchKeyword,
+      setSearchKeyword,
+      ...sortingProps
+    } = stateProps;
 
-  return (
-    <View>
-      <RepositoryListFilter
-        searchKeyword={searchKeyword}
-        setSearchKeyword={setSearchKeyword}
-      />
-      <RepositoryListSortingPicker {...sortingProps} />
-    </View>
-  );
-};
+    return (
+      <View>
+        <RepositoryListFilter
+          searchKeyword={searchKeyword}
+          setSearchKeyword={setSearchKeyword}
+        />
+        <RepositoryListSortingPicker {...sortingProps} />
+      </View>
+    );
+  };
 
-export default RepositoryListHeaderContainer;
+  render() {
+    return this.renderHeader();
+  }
+}
+
+// import { View } from "react-native"
+// import RepositoryListSortingPicker from "./RepositoryListSortingPicker"
+// import RepositoryListFilter from "./RepositoryListFilter";
+//
+// const RepositoryListHeaderContainer = (stateProps) => {
+//   const {
+//     searchKeyword,
+//     setSearchKeyword,
+//     ...sortingProps
+//   } = stateProps;
+
+//   return (
+//     <View>
+//       <RepositoryListFilter
+//         searchKeyword={searchKeyword}
+//         setSearchKeyword={setSearchKeyword}
+//       />
+//       <RepositoryListSortingPicker {...sortingProps} />
+//     </View>
+//   );
+// };
+
+// export default RepositoryListHeaderContainer;
