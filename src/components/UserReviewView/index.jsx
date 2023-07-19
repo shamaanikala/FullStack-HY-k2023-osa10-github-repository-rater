9c64981977426c3c0ac98815169272b107e1c23d
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-19 14:28:55 +0300

    Ex10.25 add <UserReviewView>

---
+++ src/components/UserReviewView/index.jsx
@@ -0,0 +1,14 @@
+import { View } from "react-native"
+import Text from "../Text"
+
+const UserReviewView = () => {
+  return (
+    <View>
+      <Text>
+        User Review View
+      </Text>
+    </View>
+  );
+};
+
+export default UserReviewView;
