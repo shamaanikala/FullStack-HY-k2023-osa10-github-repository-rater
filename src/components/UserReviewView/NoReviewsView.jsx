473ca0b02c2fccf4ed48cd4f20cc8d0ed0aede4f
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-19 18:19:54 +0300

    Add create a review button to empty reviews

--- src/components/UserReviewView/NoReviewsView.jsx
+++ src/components/UserReviewView/NoReviewsView.jsx
@@ -1,6 +1,7 @@
-import { StyleSheet, View } from "react-native"
+import { Button, StyleSheet, View } from "react-native"
 import Text from "../Text";
 import theme from "../../theme";
+import { useNavigate } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -32,15 +33,30 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 10,
     borderColor: theme.colors.primary,
   },
+  middleContainer: {
+    alignItems: 'center',
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderColor: theme.colors.primary,
+  },
   firstLineText: {
     paddingVertical: 10,
   },
   secondLineText: {
     fontStyle: 'italic',
   },
+  middleLineText: {
+    marginTop: 15,
+    fontStyle: 'italic',
+  },
+  button: {
+
+  },
 });
 
 const NoReviewsView = () => {
+  const navigate = useNavigate();
+
   return (
     <View style={styles.container}>
       <View style={styles.firstLineContainer}>
@@ -48,16 +64,21 @@ const NoReviewsView = () => {
           You have not reviewed any GitHub repositories yet!
         </Text>
       </View>
+      <View style={styles.middleContainer}>
+        <Button
+          onPress={() => navigate('/newreview')}
+          title="Create a review"
+          style={styles.button}
+          color={theme.colors.primary}
+        />
+      </View>
       <View style={styles.secondLineContainer}>
-        <Text style={styles.secondLineText}>
-          Create a new GitHub repository review by pressing the
-        </Text>
+        <Text style={styles.middleLineText}>or press</Text>
         <Text fontSize="subheading" fontWeight="bold" style={styles.firstLineText}>Create a review</Text>
         <Text style={styles.secondLineText}>
           tab from top navigation bar.
         </Text>
       </View>
-
     </View>
   );
 };
