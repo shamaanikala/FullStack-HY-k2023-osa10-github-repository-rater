72d16e4d8e9908a33eef366a1005abe1e419d9fa
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-19 16:13:04 +0300

    10.25 add basic styles to no reviews view

--- src/components/UserReviewView/NoReviewsView.jsx
+++ src/components/UserReviewView/NoReviewsView.jsx
@@ -5,17 +5,34 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: 'gold',
   },
+  firstLineContainer: {
+    backgroundColor: 'beige',
+    borderWidth: 1,
+  },
+  secondLineContainer: {
+    backgroundColor: 'cornsilk',
+    borderWidth: 1,
+  },
+  firstLineText: {},
+  secondLineText: {
+    fontStyle: 'italic',
+  },
 });
 
 const NoReviewsView = () => {
   return (
     <View style={styles.container}>
-      <Text>
-        You have not reviewed any GitHub repositories yet!
-      </Text>
-      <Text>
-        Create a new GitHub repository review from the `&quot;`Create a review `&quot;` tab.
-      </Text>
+      <View style={styles.firstLineContainer}>
+        <Text style={styles.firstLineText}>
+          You have not reviewed any GitHub repositories yet!
+        </Text>
+      </View>
+      <View style={styles.secondLineContainer}>
+        <Text style={styles.secondLineText}>
+          Create a new GitHub repository review from the `&quot;`Create a review `&quot;` tab.
+        </Text>
+      </View>
+
     </View>
   );
 };
