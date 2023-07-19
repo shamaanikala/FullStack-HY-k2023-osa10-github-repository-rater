bb7f49c790090aa64fdbd9b961bbc306067605ec
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-19 15:04:55 +0300

    10.25 userQuery to <MyReviewView>

--- src/components/UserReviewView/index.jsx
+++ src/components/UserReviewView/index.jsx
@@ -1,7 +1,19 @@
 import { View } from "react-native"
 import Text from "../Text"
+import { useQuery } from "@apollo/client";
+import { GET_SIGNED_USER } from "../../graphql/queries";
 
 const UserReviewView = () => {
+  const userQuery = useQuery(GET_SIGNED_USER, {
+    variables: { includeReviews: true },
+  });
+
+  if (userQuery.loading) {
+    console.log('loading...');
+  } else {
+    console.log(userQuery.data);
+  }
+
   return (
     <View>
       <Text>
