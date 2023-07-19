7856f6ac423214aed5ac94c39192e16c90bc14f1
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-19 10:01:43 +0300

    10.24 searchKeyword state to <RepoList>

--- src/components/RepositoryList/index.jsx
+++ src/components/RepositoryList/index.jsx
@@ -3,13 +3,16 @@ import { useState } from "react";
 import RepositoryListContainer from "./RepositoryListContainer";
 
 const RepositoryList = () => {
+  // Picker states and variables
   const [orderBy, setOrderBy] = useState('CREATED_AT');
   const [orderDirection, setOrderDirection] = useState('DESC');
   const [selectedOption, setSelectedOption] = useState('latest');
+  // repository filtering states and variables
+  const [searchKeyword, setSearchKeyword] = useState('');
 
-  const variables = { orderBy, orderDirection };
+  const variables = { orderBy, orderDirection, searchKeyword };
 
-  const { repositories } = orderBy || orderDirection
+  const { repositories } = orderBy || orderDirection || searchKeyword
     ? useRepositories(variables)
     : useRepositories();
 
