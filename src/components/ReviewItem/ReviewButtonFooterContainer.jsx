eb949ef67255214cf0aa470ac5e3919a19f484c3
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-07-19 18:54:13 +0300

    10.26 non-functional buttons

--- src/components/ReviewItem/ReviewButtonFooterContainer.jsx
+++ src/components/ReviewItem/ReviewButtonFooterContainer.jsx
@@ -1,12 +1,42 @@
-import { View } from "react-native"
-import Text from "../Text"
+import { Button, View, StyleSheet } from "react-native"
 
-const ReviewButtonFooterContainer = () => {
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    paddingBottom: 15,
+  },
+  button: {
+    // fontSize: theme.fontSizes.header,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+});
+
+const ViewRepositoryButton = ({ repositoryId }) => {
   return (
-    <View>
-      <Text>
-        Review Buttons here
-      </Text>
+    <Button
+      onPress={() => console.log(`View repository: ${repositoryId}`)}
+      title="View repository"
+    />
+  );
+};
+const DeleteReviewButton = ({ reviewId }) => {
+  return (
+    <Button
+      onPress={() => console.log(`Delete review: ${reviewId}`)}
+      title="Delete review"
+      color="red"
+    />
+  );
+};
+const ReviewButtonFooterContainer = ({ repositoryId = 'dummy-repo-id', reviewId = 'dummy-review-id' }) => {
+  return (
+    <View style={styles.container}>
+      <ViewRepositoryButton style={styles.button} repositoryId={repositoryId} />
+      <DeleteReviewButton style={styles.button} reviewId={reviewId} />
     </View>
   );
 };
